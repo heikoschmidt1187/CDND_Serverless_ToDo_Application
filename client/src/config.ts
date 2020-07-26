c4bda6d6849b4a74138d6268f13ffffcb35dee14
heikoschmidt1187 <github@heikoschmidt.info>
2020-07-26 10:32:34 +0200

    configured frontend

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,7 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-6fg0e-ow.eu.auth0.com',            // Auth0 domain
+  clientId: '58UD9HdRFenp7YnHnWBnlxc7Lhc1rkU6',   // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
