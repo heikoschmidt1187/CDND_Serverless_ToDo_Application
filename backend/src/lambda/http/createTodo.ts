be2c970e4ee60973ddf4b56cf3a8fd1d13293d37
heikoschmidt1187 <github@heikoschmidt.info>
2020-07-26 16:07:05 +0200

    finished creation of todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -2,11 +2,35 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
+import { createTodo } from '../../businessLogic/todos'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+// instantiate logger
+const logger = createLogger('createTodo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  logger.info('Processing event to create new TODO item', {event})
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  // retrieve user ID
+  const userId = getUserId(event)
+
+  // create TODO request
+  const newTodoItem: CreateTodoRequest = JSON.parse(event.body)
+
+  // create the item 
+  const newItem = await createTodo(userId, newTodoItem)
+
+  // respond
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      item: newItem
+    })
+  }
 }
